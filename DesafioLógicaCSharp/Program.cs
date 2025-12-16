56246226a3f8a5f488184c2d4096692f1998d181
Felipe Luizon <luizonfelipe@gmail.com>
2025-12-15 22:51:10 -0300

    feat: implement exercise 1 (welcome message)

--- DesafioLógicaCSharp/Program.cs
+++ DesafioLógicaCSharp/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main() 
     {
-    
+        Exercicio01_BoasVindas();
+    }
+
+    static void Exercicio01_BoasVindas() 
+    {
+        Console.Write("Digite seu nome: ");
+        string nome = Console.ReadLine() ?? "";
+        Console.WriteLine($"Olá, {nome}! Seja muito bem-vindo(a)!");
     }
 }
